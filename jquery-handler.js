9a106aa2fae1e8cafd10cc3dfd0e386e1b822f2f
David Paniz <davidpaniz@gmail.com>
2010-06-15 13:23:09 -0300

    Second try. Finish to Jquery

--- jquery-handler.js
+++ jquery-handler.js
@@ -1,8 +1,24 @@
-$.each(['loading', 'success', 'complete', 'failure'], function(i, eventName){
-	$('['+eventName+']').each(function(index, el){
+jQuery(function ($) {
+	$.each(['loading', 'complete'], function(i, eventName){
+		$('['+eventName+']').each(function(index, el){
+			var new_el = $(el);
+			new_el.bind('ajax:'+eventName, function(xhr){
+				eval(new_el.attr(eventName));
+			});
+		});
+	});
+	
+	$('[success]').each(function(index, el){
 		var new_el = $(el);
-		new_el.bind('ajax:'+eventName, function(xhr){
-			eval(new_el.attr(eventName));
+		new_el.bind('ajax:success', function(xhr, body, status){
+			eval(new_el.attr('success'));
+		});
+	});
+	
+	$('[failure]').each(function(index, el){
+		var new_el = $(el);
+		new_el.bind('ajax:failure', function(xhr, status, error){
+			eval(new_el.attr('failure'));
 		});
 	});
 });
